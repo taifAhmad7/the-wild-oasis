8ded18655331574514d00d3865dad0a7f83e68bb
Taif Ahmad <a7x.taif@gmail.com>
2023-08-16 16:09:50 +0530

    add login credentials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("ahmadtaif747@gmailcom");
+  const [password, setPassword] = useState("pass987");
 
   const { login, isLoading } = useLogin();
 
